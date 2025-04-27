9a2d49e1ed31b3867d8210ce3ea7993417816533
mariellyn <mariellyn@hotmail.com>
2025-04-28 00:19:52 +0100

    init web project

---
+++ SalesCet96.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesCet96.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
